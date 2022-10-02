d1be2eb3f711d7c45ddc24f737f71bb735f91a03
Antonio Vukalović <ddujmov123@gmail.com>
2022-10-02 17:48:23 +0200

    commit

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/profile-app/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
 
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
